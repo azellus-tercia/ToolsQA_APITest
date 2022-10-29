99f5e233d99253a5d31ba78a13af7e2d134c5efc
Azellus Tercia <azellustercia@gmail.com>
2022-10-29 21:19:08 +0500

    Test CI/CD.

--- src/test/java/AccountTest.java
+++ src/test/java/AccountTest.java
@@ -5,12 +5,21 @@ import model.CreateUserPostJson;
 import model.JsonSuccessResponse;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-import runner.BaseRunner;
+import runner.BaseTest;
 import runner.EndPoints;
 
-public final class AccountTest extends BaseRunner {
+public final class AccountTest extends BaseTest {
     private String newUserId;
     private String newTokenAPI;
+
+    @Test
+    public void testGetResponseJson() {
+        Response response = requestSpec()
+                .body(getJSONObject("request.json"))
+                .post(EndPoints.PAGE_ACCOUNT_USER);
+
+        Assert.assertNotNull(response);
+    }
 
     @Test (description = "Отправляем запрос и получаем ответ от сервера")
     public void testGetResponse() {
